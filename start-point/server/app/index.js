6239f8c0f51449a582e43424de0a488c8815022a
kaylafuchs <kfuchs24@gmail.com>
2016-08-30 16:11:15 -0400

    made something work wooo

--- start-point/server/app/index.js
+++ start-point/server/app/index.js
@@ -2,14 +2,49 @@
 
 var app = require('express')()
 var path = require('path')
+var session = require('express-session')
+var User = require('../api/users/user.model')
 
 app.use(require('./logging.middleware'))
 
 app.use(require('./request-state.middleware'))
 
+app.use(session({
+	secret: 'lolwut'
+}))
+
+app.use(function (req, res, next) {
+  console.log('session', req.session);
+  next();
+});
+
 app.use(require('./statics.middleware'))
 
+app.use('/api', function (req, res, next) {
+  if (!req.session.counter) req.session.counter = 0;
+  console.log('request body', ++req.session.counter);
+  next();
+});
+
 app.use('/api', require('../api/api.router'))
+
+
+app.post('/login', function (req, res, next) {
+  User.findOne({
+    where: req.body
+  })
+  .then(function (user) {
+    if (!user) {
+      res.sendStatus(401);
+    } else {
+      req.session.userId = user.id;
+      console.log("user id",req.session.userId);
+      res.sendStatus(204);
+    }
+  })
+  .catch(next);
+});
+
 
 var validFrontendRoutes = ['/', '/stories', '/users', '/stories/:id', '/users/:id', '/signup', '/login']
 var indexPath = path.join(__dirname, '..', '..', 'public', 'index.html')
@@ -20,5 +55,9 @@ validFrontendRoutes.forEach(function (stateRoute) {
 })
 
 app.use(require('./error.middleware'))
+
+
+
+
 
 module.exports = app
